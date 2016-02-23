6888ba2f9b6f7ab5e10db6f4204ce81962991b43
SunMoon <sunmoon@ymnas.local>
2016-02-23 10:17:14 +0800

    Handle add remove actors, save and discard movie info change, larger rating stars

--- ui/detail/movie-detail-controller.js
+++ ui/detail/movie-detail-controller.js
@@ -4,7 +4,23 @@ angular.module('jamm')
 
     var movieId = $stateParams.id;
     if (movieId) {
-        $scope.movie = _.find($scope.movies, { id: movieId });
+        $scope.originalMovie = _.find($scope.movies, { id: movieId });
+        $scope.movie = _.cloneDeep($scope.originalMovie);
     }
+
+    $scope.isModified = false;
+
+    $scope.$watch('movie', function (value) {
+        $scope.isModified = !_.isEqual(value, $scope.originalMovie);
+    }, true);
+
+    $scope.save = function() {
+        _.assign($scope.originalMovie, $scope.movie);
+        $scope.isModified = false;
+    };
+
+    $scope.discard = function() {
+        $scope.movie = _.cloneDeep($scope.originalMovie);
+    };
 
 });
