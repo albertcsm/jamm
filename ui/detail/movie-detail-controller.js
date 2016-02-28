6d19ac057734ecf39987ed234a7d1444ff3c09ad
SunMoon <sunmoon@ymnas.local>
2016-02-29 03:54:45 +0800

    Prompt for unsaved change

--- ui/detail/movie-detail-controller.js
+++ ui/detail/movie-detail-controller.js
@@ -1,5 +1,5 @@
 angular.module('jamm')
-.controller('MovieDetailController', function ($scope, MovieService, $stateParams, $state) {
+.controller('MovieDetailController', function ($scope, MovieService, $stateParams, $state, $uibModal) {
     var movies = MovieService.movies;
 
     var movieId = $stateParams.id;
@@ -22,15 +22,35 @@ angular.module('jamm')
 
     $scope.discard = function() {
         $scope.movie = angular.copy($scope.originalMovie);
+        $scope.isModified = false;
     };
 
     $scope.delete = function () {
         _.remove(movies, { id: $scope.originalMovie.id });
         $('#confirmDeleteModal').on('hidden.bs.modal', function () {
+            $scope.originalMovie = null;
+            $scope.movie = null;
             $state.go('movies');
         });
         $('#confirmDeleteModal').modal('hide');
     };
+
+    $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, options) {
+        if ($scope.movie && $scope.isModified) {
+            event.preventDefault();
+            $uibModal.open({
+                templateUrl: 'unsaved-change-modal-template',
+                controller: 'UnsavedChangeModalController'
+            }).result.then(function (saving) {
+                if (saving) {
+                    $scope.save();
+                } else {
+                    $scope.discard();
+                }
+                $state.go(toState, toParams);
+            });
+        }
+    });
 
     $scope.selectedVideoIndex = 0;
 
@@ -50,4 +70,17 @@ angular.module('jamm')
         });
     };
 
+})
+.controller('UnsavedChangeModalController', function ($scope, $uibModalInstance) {
+    $scope.saveAndProceed = function () {
+        $uibModalInstance.close(true);
+    };
+
+    $scope.proceedWithoutSave = function () {
+        $uibModalInstance.close(false);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
 });
