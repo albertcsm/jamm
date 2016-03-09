3c1957051f03527580cc7e1d6990e61ba3cf1e7e
SunMoon <sunmoon@ymnas.local>
2016-03-10 02:24:46 +0800

    Design layout of import page

--- ui/import/movie-import-controller.js
+++ ui/import/movie-import-controller.js
@@ -1,5 +1,5 @@
 angular.module('jamm')
-.controller('MovieImportController', function ($scope, Movie, Repository) {
+.controller('MovieImportController', function ($scope, $uibModal, Movie, Repository) {
     $scope.repositories = Repository.query(function () {
         var repoId = $scope.repositories[0]._id;
         $scope.repositories[0].files = Repository.files({ id: repoId }, function () {
@@ -29,4 +29,61 @@ angular.module('jamm')
             node.children = [ {} ];
         }
     };
+
+    $scope.movie = { 
+        "_id": "HUNGER_GAME-3_1",
+        "name": "The Hunger Games: Mockingjay – Part 1",
+        "actors": [ 
+            { "name": "Jennifer Lawrence" },
+            { "name": "Josh Hutcherson" },
+            { "name": "Liam Hemsworth" }
+        ],
+        "releaseDate": "2014-11-10",
+        "rating": 2,
+        "tags": [ "Adventure", "Sci-Fi" ],
+        "storage": {
+            "repository": "home",
+            "path": "Downloads/bt/The Hunger Games Mockingjay Part 1 (2014) HDRip XviD-MAXSPEED",
+            "cover": "media/cover.jpg",
+            "images": [
+                { "file": "media/gallery1.jpg" },
+                { "file": "media/gallery2.jpg" }
+            ],
+            "videos": [
+                { 
+                    "file": "media/video1.mp4",
+                    "resolution": "720x480",
+                    "length": "05:10",
+                    "size": 33387486
+                },
+                {
+                    "file": "media/video2.mkv",
+                    "resolution": "1280x688",
+                    "length": "2:06:35",
+                    "size": 1626060492
+                }
+            ],
+            "quality": "HD"
+        }
+    };
+
+    $scope.import = function() {
+        $uibModal.open({
+            size: 'lg',
+            templateUrl: 'movie-info-modal-template',
+            controller: 'MovieImportModalController'
+        }).result.then(function () {
+            
+        });
+    };
+})
+.controller('MovieImportModalController', function ($scope, $uibModalInstance) {
+    $scope.save = function () {
+        $uibModalInstance.close(true);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
 });
+
