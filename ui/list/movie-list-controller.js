13ed065d4bbe5c90eacacb4466b412655edee01b
SunMoon <albertcsm@gmail.com>
2016-03-28 17:55:01 +0800

    calculate filter option statistics for faceted search

--- ui/list/movie-list-controller.js
+++ ui/list/movie-list-controller.js
@@ -1,54 +1,125 @@
 angular.module('jamm')
 .controller('MovieListController', function ($scope, Movie) {
-    function loadData() {
+    function loadMovies() {
         Movie.query(function (movies) {
             $scope.movies = movies;
+            $scope.filteredMovies = filterMovies($scope.filter, $scope.movies);
+            loadStatistics();
+        });
+    }
 
-            var years = {};
-            var categories = {};
-            var ratings = { };
+    function filterMovies(filter, movies) {
+        if (!filter) {
+            return movies;
+        } else {
+            var filteredMovies = [];
             angular.forEach(movies, function (movie) {
-                if (movie.releaseDate) {
-                    var year = moment(movie.releaseDate).year();
-                    if (years.hasOwnProperty(year)) {
-                        years[year]++;
-                    } else {
-                        years[year] = 1;
+                if (filter.year) {
+                    if (!movie.releaseDate) {
+                        return;
+                    }
+                    if (moment(movie.releaseDate).year() != filter.year) {
+                        return;
                     }
                 }
-                if (movie.categories) {
-                    angular.forEach(movie.categories, function (movieCategory) {
-                        if (categories.hasOwnProperty(movieCategory.name)) {
-                            categories[movieCategory.name]++;
-                        } else {
-                            categories[movieCategory.name] = 1;
-                        }
-                    });
+
+                if (filter.category) {
+                    if (!movie.categories) {
+                        return;
+                    }
+                    if (movie.categories && _.filter(movie.categories, { name: filter.category }).length == 0) {
+                        return;
+                    }
                 }
-                var rating = movie.rating ? movie.rating : 0;
-                if (ratings.hasOwnProperty(rating)) {
-                    ratings[rating]++;
-                } else {
-                    ratings[rating] = 1;
+
+                if (filter.rating === 0 || filter.rating) {
+                    var filterRating = filter.rating + 0;
+                    var movieRating = movie.rating ? movie.rating : 0;
+                    if (filterRating != movieRating) {
+                        return;
+                    }
                 }
+
+                filteredMovies.push(movie);
             });
-            $scope.years = _.map(years, function(value, key) {
-                return { key: key, count: value };
-            });
-            $scope.categories = _.map(categories, function(value, key) {
-                return { key: key, count: value };
-            });
-            $scope.ratings = ratings;
+            return filteredMovies;
+        }
+    }
+
+    function loadStatistics() {
+        var years = {};
+        var categories = {};
+        var ratings = {};
+        angular.forEach($scope.movies, function (movie) {
+            if (movie.releaseDate) {
+                var year = moment(movie.releaseDate).year();
+                if (!years.hasOwnProperty(year)) {
+                    years[year] = 0;
+                } 
+            }
+            if (movie.categories) {
+                angular.forEach(movie.categories, function (movieCategory) {
+                    if (!categories.hasOwnProperty(movieCategory.name)) {
+                        categories[movieCategory.name] = 0;
+                    }
+                });
+            }
+            var rating = movie.rating ? movie.rating : 0;
+            if (!ratings.hasOwnProperty(rating)) {
+                ratings[rating] = 0;
+            }
         });
+
+        // faceted search statistics for year filter
+        var moviesIgnoreYearFilter = filterMovies(_.omit($scope.filter, 'year'), $scope.movies);
+        angular.forEach(moviesIgnoreYearFilter, function (movie) {
+            if (movie.releaseDate) {
+                var year = moment(movie.releaseDate).year();
+                years[year]++;
+            }
+        });
+        $scope.years = _.map(years, function(value, key) {
+            return { key: key, count: value };
+        });
+
+        // faceted search statistics for category filter
+        var moviesIgnoreCategoryFilter = filterMovies(_.omit($scope.filter, 'category'), $scope.movies);
+        angular.forEach(moviesIgnoreCategoryFilter, function (movie) {
+            if (movie.categories) {
+                angular.forEach(movie.categories, function (movieCategory) {
+                    categories[movieCategory.name]++;
+                });
+            }
+        });
+        $scope.categories = _.map(categories, function(value, key) {
+            return { key: key, count: value };
+        });
+
+        // faceted search statistics for rating filter
+        var moviesIgnoreRatingFilter = filterMovies(_.omit($scope.filter, 'rating'), $scope.movies);
+        angular.forEach(moviesIgnoreRatingFilter, function (movie) {
+            var rating = movie.rating ? movie.rating : 0;
+            ratings[rating]++;
+        });
+        $scope.ratings = ratings;
     }
 
     $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, options) {
         if (toState.name == 'movies.list') {
-            loadData();
+            loadMovies();
         }
     });
 
-    loadData();
+    $scope.$watch('filter', function () {
+        $scope.filteredMovies = filterMovies($scope.filter, $scope.movies);
+        loadStatistics();
+    }, true);
+
+    loadMovies();
+
+    $scope.resetFilter = function () {
+        $scope.filter = {};
+    }
 
     $scope.displayStyle = 'thumbnail';
     $scope.sortParam = {
