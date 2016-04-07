b62366befdb80076ab09c9914953b5667c8f9397
SunMoon <albertcsm@gmail.com>
2016-04-07 09:22:17 +0800

    build inverted index for faceted search

--- ui/components/faceted-search/faceted-search-directive.js
+++ ui/components/faceted-search/faceted-search-directive.js
@@ -1,78 +1,63 @@
 angular.module('jamm.facetedSearch', [ ])
 .directive('jammFacetedSearch', function() {
 
-    function filterData(mappedValueSets, filterSet) {
-        var filteredData = [];
-        for (var id in mappedValueSets) {
-            var mappedValueSet = mappedValueSets[id];
-
-            var discard = false;
-            for (var filterName in filterSet) {
-                var filterValue = filterSet[filterName];
-                if (filterValue) {
-                    var mappedValue = mappedValueSet[filterName];
-                    if (Array.isArray(mappedValue)) {
-                        var found = false;
-                        if (mappedValue.indexOf(filterValue) == -1) {
-                            discard = true;
-                            break;
-                        }
-                    } else {
-                        if (mappedValue != filterValue) {
-                            discard = true;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            if (!discard) {
-                filteredData.push(mappedValueSet);
-            }
-        }
-        return filteredData;
-    }
-
-    function loadStatistics(mappedValueSets, filterSet, filterTemplates) {
-        var statistics = {};
-
-        for (var i = 0; i < filterTemplates.length; i++) {
-            var filterTemplate = filterTemplates[i];
-
-            var filteredData = filterData(mappedValueSets, _.omit(filterSet, filterTemplate.name));
-            var histogram = {};
-            for (var id in filteredData) {
-                var mappedValueSet = filteredData[id];
-                var mappedValue = mappedValueSet[filterTemplate.name];
-                if (Array.isArray(mappedValue)) {
-                    for (var j = 0; j < mappedValue.length; j++) {
-                        histogram[mappedValue[j]] = (histogram[mappedValue[j]] | 0) + 1;
-                    }
-                } else {
-                    histogram[mappedValue] = (histogram[mappedValue] | 0) + 1;
-                }
-            }
-            statistics[filterTemplate.name] = _.map(histogram, function(value, key) {
-                return { key: key, count: value };
-            });;
-        }
-        return statistics;
-    }
-
     function link(scope, element, attrs) {
         scope.statistics = {};
         scope.filterSet = {};
-        var mappedValueSets = {};
-        var itemIndex = {};
+        var invertedIndexMap = {};
+
+        function getFilteredItems(filterSet) {
+            if (filterSet && !angular.equals(filterSet, {})) {
+                var filteredItems = [];
+                angular.forEach(scope.items, function (item) {
+                    var survive = true;
+                    for (var filterName in filterSet) {
+                        var filterValue = filterSet[filterName];
+                        if (filterValue) {
+                            var itemArray = invertedIndexMap[filterName][filterValue];
+                            if (!itemArray || itemArray.indexOf(item) == -1) {
+                                survive = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (survive) {
+                        filteredItems.push(item);
+                    }
+                });
+                return filteredItems;
+            } else {
+                return scope.items;
+            }
+        }
+
+        function updateStatistics() {
+            scope.statistics = {};
+            for (var i = 0; i < scope.filterTemplates.length; i++) {
+                var filterTemplate = scope.filterTemplates[i];
+
+                var filteredItems = getFilteredItems(_.omit(scope.filterSet, filterTemplate.name));
+
+                var histogram = {};
+                var invertedIndex = invertedIndexMap[filterTemplate.name];
+                for (var filterValue in invertedIndex) {
+                    var itemArray = invertedIndex[filterValue];
+                    if (itemArray) {
+                        var count = _.intersection(itemArray, filteredItems).length;
+                        if (count) {
+                            histogram[filterValue] = count;
+                        }
+                    }
+                }
+                scope.statistics[filterTemplate.name] = _.map(histogram, function(value, key) {
+                    return { key: key, count: value };
+                });;
+            }
+        }
 
         function applyFilters() {
-            var filteredData = filterData(mappedValueSets, scope.filterSet);
-            scope.statistics = loadStatistics(mappedValueSets, scope.filterSet, scope.filterTemplates);
-            var filteredItems = [];
-            for (var i = 0; i < filteredData.length; i++) {
-                var filteredId = filteredData[i]._id;
-                filteredItems.push(itemIndex[filteredId]);
-            }
+            updateStatistics();
+            var filteredItems = getFilteredItems(scope.filterSet);
             scope.searchResultCallback({ items: filteredItems });
         }
 
@@ -85,17 +70,31 @@ angular.module('jamm.facetedSearch', [ ])
         }, true);
 
         scope.$watch('items', function () {
-            mappedValueSets = {};
-            itemIndex = {};
+            invertedIndexMap = {};
             angular.forEach(scope.items, function (item) {
                 var id = item[scope.idField];
-                var mappedValueSet = {};
+
                 angular.forEach(scope.filterTemplates, function (filterTemplate) {
-                    mappedValueSet[filterTemplate.name] = filterTemplate.valueMapper(item);
+                    if (!invertedIndexMap.hasOwnProperty(filterTemplate.name)) {
+                        invertedIndexMap[filterTemplate.name] = {};
+                    }
+                    var invertedIndexForField = invertedIndexMap[filterTemplate.name];
+                    var mappedValue = filterTemplate.valueMapper(item);
+                    if (mappedValue) {
+                        function addToIndex(value) {
+                            if (!invertedIndexForField[value]) {
+                                invertedIndexForField[value] = [];
+                            }
+                            invertedIndexForField[value].push(item);
+                        }
+
+                        if (Array.isArray(mappedValue)) {
+                            angular.forEach(mappedValue, addToIndex);
+                        } else {
+                            addToIndex(mappedValue);
+                        }
+                    }
                 });
-                mappedValueSet._id = id;
-                mappedValueSets[id] = mappedValueSet;
-                itemIndex[id] = item;
             });
             applyFilters();
         }, true);
